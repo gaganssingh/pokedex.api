d96119c930f642d0b0c4ed0e927ab171229c9f88
Gagan Singh <gagansingh.dev@outlook.com>
2020-01-08 13:11:37 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+	res.send("Hello, world!");
+});
+
+const PORT = 8000;
+app.listen(PORT, () => `Server listening at http://localhost:${PORT}`);
